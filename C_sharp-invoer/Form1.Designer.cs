97388afb61b5afd266836189352304c871040317
Tristán <Tristan.Nouwens@student.gildeopleidingen.nl>
2024-10-01 10:40:05 +0200

    V1

---
+++ C_sharp-invoer/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace C_sharp_invoer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(103, 93);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(100, 26);
+            this.txtFirstName.TabIndex = 0;
+            this.txtFirstName.TextChanged += new System.EventHandler(this.txtFristName_TextChanged);
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(449, 93);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(100, 26);
+            this.txtLastName.TabIndex = 1;
+            this.txtLastName.TextChanged += new System.EventHandler(this.txtLastName_TextChanged);
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(282, 255);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(129, 93);
+            this.btnSubmit.TabIndex = 2;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.txtFirstName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.Button btnSubmit;
+    }
+}
+
---
+++ C_sharp-invoer/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions; // nodig voor Regex
+
+namespace C_sharp_invoer
+{
+    public partial class Form1 : Form
+    {
+        // Gejat van internet, want wil geen domme antwoorden
+        private static readonly Regex nameRegex = new Regex(@"^[A-Za-zÀ-ÿ'-]+$", RegexOptions.Compiled);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtFristName_TextChanged(object sender, EventArgs e)
+        {
+            // Doe niks 
+        }
+
+        private void txtLastName_TextChanged(object sender, EventArgs e)
+        {
+            // Doet ook niks
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            // is ie leeg?
+            if (string.IsNullOrWhiteSpace(txtFirstName.Text))
+            {
+                MessageBox.Show("Invalid first name");
+                return;
+            }
+
+            // is het een juist format?
+            if (!nameRegex.IsMatch(txtFirstName.Text))
+            {
+                MessageBox.Show("First name must contain only letters");
+                return;
+            }
+
+            // Is deze leeg?
+            if (string.IsNullOrWhiteSpace(txtLastName.Text))
+            {
+                MessageBox.Show("Invalid last name");
+                return;
+            }
+
+            // is deze het juiste format?
+            if (!nameRegex.IsMatch(txtLastName.Text))
+            {
+                MessageBox.Show("Last name must contain only letters");
+                return;
+            }
+
+            // Wow alles is goed ingevuld nu display!
+            MessageBox.Show(txtFirstName.Text + " " + txtLastName.Text);
+        }
+    }
+}
